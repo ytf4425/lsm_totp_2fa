c5b4ee9d8a443060cd0a38f67a2a2e816c5d896f
Hanabi <93yutf@gmail.com>
2022-12-25 13:26:42 +0000

    use vmalloc

--- 2fa_module.c
+++ 2fa_module.c
@@ -3,7 +3,7 @@
 #include <linux/module.h> /* Specifically, a module */
 #include <linux/proc_fs.h> /* Necessary because we use the proc fs */
 #include <linux/string.h>
-#include <linux/slab.h>
+#include <linux/vmalloc.h>
 
 // #include <linux/seq_file.h> /* using seq_printf */
 // #include <linux/slab.h>     /* Using kzalloc */
@@ -12,22 +12,24 @@
 #define MAX_BUFF_SIZE 2048
 
 MODULE_AUTHOR("Tommy Yu");
-static char* sbuff= NULL;
-struct proc_dir_entry *dir, *fpath, *fkey, *fstate;
-char *path = "", *key = "", *state = "locked";
+static char* sbuff = NULL;
+static struct proc_dir_entry *dir, *fpath, *fkey, *fstate;
+static char *path = NULL, *key = NULL;
 
 static int unlock(void);
 static int lock(void);
 static ssize_t proc_read_state(struct file* file, char __user* buffer, size_t count, loff_t* f_pos);
 static ssize_t proc_write_state(struct file* file, const char __user* buffer, size_t count, loff_t* f_pos);
+static ssize_t proc_read_path(struct file* file, char __user* buffer, size_t count, loff_t* f_pos);
+static ssize_t proc_write_path(struct file* file, const char __user* buffer, size_t count, loff_t* f_pos);
+static ssize_t proc_write_key(struct file* file, const char __user* buffer, size_t count, loff_t* f_pos);
 
 static const struct proc_ops path_fops = {
-    .proc_read = proc_read_state,
-    .proc_write = proc_write_state,
+    .proc_read = proc_read_path,
+    .proc_write = proc_write_path,
 };
 static const struct proc_ops key_fops = {
-    .proc_read = proc_read_state,
-    .proc_write = proc_write_state,
+    .proc_write = proc_write_key,
 };
 
 static const struct proc_ops state_fops = {
@@ -52,12 +54,16 @@ static int __init proc_2fa_init(void)
     }
 
     /* init buff */
-    // sbuff = (char*)vmalloc(MAX_BUFF_SIZE);
-    // if (!sbuff)
-    //     return -ENOMEM;
-    // else
-    //     memset(sbuff, 0, MAX_BUFF_SIZE);
-    // strcpy(sbuff, "aaa");
+    sbuff = (char*)vmalloc(MAX_BUFF_SIZE);
+    path = (char*)vmalloc(MAX_BUFF_SIZE);
+    key = (char*)vmalloc(MAX_BUFF_SIZE);
+    if (!sbuff || !path || !key)
+        return -ENOMEM;
+    else {
+        memset(sbuff, 0, MAX_BUFF_SIZE);
+        memset(path, 0, MAX_BUFF_SIZE);
+        memset(key, 0, MAX_BUFF_SIZE);
+    }
 
     return 0;
 }
@@ -65,13 +71,67 @@ static int __init proc_2fa_init(void)
 static void __exit proc_2fa_exit(void)
 {
     proc_remove(dir);
-    // vfree(sbuff);
+    vfree(sbuff);
+    vfree(path);
+    vfree(key);
     printk(KERN_INFO "[proc_2fa] module exit.\n");
+}
+
+static ssize_t proc_write_key(struct file* file, const char __user* buffer, size_t count, loff_t* f_pos)
+{
+    count = count < MAX_BUFF_SIZE ? count : MAX_BUFF_SIZE;
+
+    if (copy_from_user(sbuff, buffer, count)) { // error
+        printk(KERN_INFO "[proc_2fa]: copy_from_user() error!\n");
+        return -EFAULT;
+    }
+
+    sscanf(sbuff, "%s", key);
+
+    if (strlen(key)!=6) {
+        printk(KERN_INFO "[proc_2fa]: /proc/2fa/key got unavaliable input.\n");
+        return -EFAULT;
+    }
+    return count;
+}
+
+static ssize_t proc_read_path(struct file* file, char __user* buffer, size_t count, loff_t* f_pos)
+{
+    count = strlen(path);
+    if (*f_pos >= count) {
+        return 0;
+    }
+    count -= *f_pos;
+    if (copy_to_user(buffer, path + *f_pos, count)) {
+        return -EFAULT;
+    }
+    *f_pos += count;
+    return count;
+}
+
+static ssize_t proc_write_path(struct file* file, const char __user* buffer, size_t count, loff_t* f_pos)
+{
+    count = count < MAX_BUFF_SIZE ? count : MAX_BUFF_SIZE;
+
+    if (copy_from_user(sbuff, buffer, count)) { // error
+        printk(KERN_INFO "[proc_2fa]: copy_from_user() error!\n");
+        return -EFAULT;
+    }
+
+    sscanf(sbuff, "%s", path);
+
+    // TODO: check path
+    if (0) {
+        printk(KERN_INFO "[proc_2fa]: /proc/2fa/path got unavaliable input.\n");
+        return -EFAULT;
+    }
+    return count;
 }
 
 static ssize_t proc_read_state(struct file* file, char __user* buffer, size_t count, loff_t* f_pos)
 {
-    count = strlen(state);
+    // TODO: read state to sbuff
+    count = strlen(sbuff);
     if (*f_pos >= count) {
         return 0;
     }
@@ -89,16 +149,10 @@ static ssize_t proc_write_state(struct file* file, const char __user* buffer, si
 
     count = count < MAX_BUFF_SIZE ? count : MAX_BUFF_SIZE;
 
-    char* tmp = kzalloc((count + 1), GFP_KERNEL);
-    if (!tmp)
-        return -ENOMEM;
-    if (copy_from_user(tmp, buffer, count)) {
+    if (copy_from_user(sbuff, buffer, count)) { // error
         printk(KERN_INFO "[proc_2fa]: copy_from_user() error!\n");
-        kfree(tmp);
         return -EFAULT;
     }
-    kfree(sbuff);
-    sbuff = tmp;
 
     sscanf(sbuff, "%d", &new_state);
     if (new_state == 1)
@@ -115,14 +169,16 @@ static ssize_t proc_write_state(struct file* file, const char __user* buffer, si
 
 static int lock(void)
 {
-    sbuff = "lock";
+    // TODO: lock
     printk(KERN_INFO "[proc_2fa]: %s locked.\n", path);
     return 0;
 }
 
 static int unlock(void)
 {
+    // TODO: check
     if (1) {
+        // TODO: unlock
         printk(KERN_INFO "[proc_2fa]: %s unlocked.\n", path);
     } else {
         printk(KERN_INFO "[proc_2fa]: %s failed to be unlocked.\n", path);
